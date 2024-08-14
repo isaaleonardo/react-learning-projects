2620136cfe720819bb3b3dd02ab1d08a7617a82f
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-08-14 13:32:08 -0400

    Create proyect of mouse-follower

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -1,9 +1,46 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App () {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('mousemove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('mousemove', handleMove)
+    }
+  }, [enabled])
+
   return (
     <>
-      <h2>Proyecto 3</h2>
+      <main>
+        <div
+          style={{
+            position: 'absolute',
+            backgroundColor: '#09f',
+            borderRadius: '50%',
+            opacity: 0.8,
+            pointerEvents: 'none',
+            left: -20,
+            top: -20,
+            width: 40,
+            height: 40,
+            transform: `translate(${position.x}px, ${position.y}px)`
+          }}
+        />
+        <button onClick={() => setEnabled(!enabled)}>
+          {enabled ? 'Desactivar' : 'Activar'} seguidor de mouse
+        </button>
+      </main>
     </>
   )
 }
