a27479e062308887371fe56e794bf5ba24038cb6
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-08-15 12:10:34 -0400

    Create a button to reset app

--- projects/04-technical-test/src/App.jsx
+++ projects/04-technical-test/src/App.jsx
@@ -8,16 +8,22 @@ export function App () {
   const [fact, setFact] = useState('')
 
   useEffect(() => {
+    getNewFact()
+  }, [])
+
+  const getNewFact = () => {
     fetch(URL_CAT_RANDOM_FACT)
       .then(res => res.json())
       .then(data => {
-        setFact(data.fact)
+        const newFact = data.fact
+        setFact(newFact)
       })
-  }, [])
+  }
 
   return (
     <main>
       <h1>Little cats facts</h1>
+      <button onClick={getNewFact}>Get new fact</button>
       <p>{fact}</p>
       <img src={`${PREFIX_URL_CAT_IMAGE}${firstWordFrom(fact)}`} alt='Image extracted using the first word of cat fact' />
     </main>
